70103cf31214c95a017b1623cd4d6a00d4655465
omy86 <harrysytito@gmail.com>
2020-04-03 14:59:39 +0200

    import router on App.js, create CreateComments, DeleteComments dans pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import CreateComments from './pages/CreateComments';
+import DeleteComments from './pages/DeleteComments';
+
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/comments/create" component={CreateComments} />
+        <Route path="/comments/delete" component={DeleteComments} />
+      </Switch>
+    </Router>
   );
 }
 
